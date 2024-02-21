990473ab093f97736e02be8652789bfc27be56cd
wooyoung <andy241822@gmail.com>
2024-02-22 06:21:32 +0900

    [UPDATE] Scheduled 시간 변경

--- src/main/java/com/example/auction/Controller/MainController.java
+++ src/main/java/com/example/auction/Controller/MainController.java
@@ -157,7 +157,7 @@ public class MainController {
     }
     @Async
 //    @Scheduled(cron = "0 * * * * *") // 매시간 0분 0초에 실행
-    @Scheduled(cron = "0 */10 * * * *")
+    @Scheduled(cron = "0 0/10 * * * *")
     public void scheduledMethod() {
         setSec(599);
         for (int i = 0; i < no.size(); i++) {
